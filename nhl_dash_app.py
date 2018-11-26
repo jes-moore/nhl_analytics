d61e390c3513c1ffa3c98a3dce04ba00137e557d
testuser <testuser@example.com>
2018-11-26 08:29:11 -0700

    First Commit

---
+++ nhl_dash_app.py
@@ -0,0 +1,27 @@
+#Plotting Libraries
+from plotting import *
+
+#Dashboard Libraries
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure=plot_shots()
+        )
+    ])
+
+if __name__ == '__main__':
+    app.run_server(debug=False)
